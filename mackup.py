3e8cfd0bcfc063bf5d5bf2ad99c5fda798b93c80
Morton Jonuschat <m.jonuschat@gute-botschafter.de>
2013-07-22 09:15:01 +0200

    Add support for .before & .after vimrc files

--- mackup.py
+++ mackup.py
@@ -294,8 +294,12 @@ SUPPORTED_APPS = {
     'Ventrilo': [PREFERENCES + 'Ventrilo'],
 
     'Vim': ['.gvimrc',
+            '.gvimrc.before',
+            '.gvimrc.after',
             '.vim',
-            '.vimrc'],
+            '.vimrc',
+            '.vimrc.before',
+            '.vimrc.after'],
 
     'Vimperator': ['.vimperator',
                    '.vimperatorrc'],

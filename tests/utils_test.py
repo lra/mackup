48282f17dad3c2144367d58c45db19bbeda72071
Timidger <APragmaticPlace@gmail.com>
2015-02-04 12:42:39 -0500

    Added test_copy_dir, which tests recursively copying a directory to the given path

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -169,6 +169,44 @@ class TestMackup(unittest.TestCase):
         utils.delete(srcpath)
         utils.delete(dstpath)
 
+    def test_copy_dir(self):
+        """
+        Copies a directory recursively to the destination path
+        """
+        # Create a tmp folder
+        srcpath = tempfile.mkdtemp()
+
+        # Create a tmp file
+        tf = tempfile.NamedTemporaryFile(delete=False, dir=srcpath)
+        srcfile = tf.name
+        tf.close()
+
+        # Create a tmp folder
+        dstpath = tempfile.mkdtemp()
+
+        # Set the destination filename
+        srcpath_basename = os.path.basename(srcpath)
+        dstfile = os.path.join(dstpath,
+                               srcpath_basename,
+                               os.path.basename(srcfile))
+        # Make sure the source file and destination folder exist and the
+        # destination file doesn't yet exist
+        assert os.path.isdir(srcpath)
+        assert os.path.isfile(srcfile)
+        assert os.path.isdir(dstpath)
+        assert not os.path.exists(dstfile)
+
+        # Check if mackup can copy it
+        utils.copy(srcpath, dstfile)
+        assert os.path.isdir(srcpath)
+        assert os.path.isfile(srcfile)
+        assert os.path.isdir(dstpath)
+        assert os.path.exists(dstfile)
+
+        # Let's clean up
+        utils.delete(srcpath)
+        utils.delete(dstpath)
+
     def test_link_file(self):
         # Create a tmp file
         tf = tempfile.NamedTemporaryFile(delete=False)

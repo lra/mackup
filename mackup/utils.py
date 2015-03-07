675e81a11be5cb3e9b814cc8034601567ab945e8
ninjabong <ninja@ninjabong.com>
2015-03-06 22:02:50 -0500

    Fixed missing path for box engine referencing wrong variable

--- mackup/utils.py
+++ mackup/utils.py
@@ -288,7 +288,7 @@ def get_box_folder_location():
         with open(box_sync_path, 'r') as sync_path:
             box_home = sync_path.read()
     except IOError:
-        error('Unable to find your Box prefs at ' , sync_path , ' =(')
+        error('Unable to find your Box prefs at ' , box_sync_path , ' =(')
 
     return box_home
 

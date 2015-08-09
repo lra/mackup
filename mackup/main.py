3406a206cb18ed99797cc0fa8e386337a5265203
Timidger <APragmaticPlace@gmail.com>
2015-08-09 17:55:18 -0500

    Updated to docopt and removed ugly input hack

--- mackup/main.py
+++ mackup/main.py
@@ -47,6 +47,10 @@ def main():
     mckp = Mackup()
     app_db = ApplicationsDatabase()
 
+    # If we want to answer mackup with "yes" for each question
+    if args['force']:
+        utils.FORCE_YES = True
+
     if args['backup']:
         # Check the env where the command is being run
         mckp.check_for_usable_backup_env()

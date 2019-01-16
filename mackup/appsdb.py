a82b468a87808079034f9592de80e312ea275dd2
Laurent Raufaste <analogue@glop.org>
2019-01-15 21:17:10 -0800

    We should not worry if the XDG_CONFIG_HOME does not exist
    Fix #1253

--- mackup/appsdb.py
+++ mackup/appsdb.py
@@ -57,25 +57,21 @@ class ApplicationsDatabase(object):
                 home = os.path.expanduser('~/')
                 failobj = "{}.config".format(home)
                 xdg_config_home = os.environ.get('XDG_CONFIG_HOME', failobj)
-                if xdg_config_home:
-                    if not os.path.exists(xdg_config_home):
-                        raise ValueError('$XDG_CONFIG_HOME: {} does not exist'
-                                         .format(xdg_config_home))
-                    if not xdg_config_home.startswith(home):
-                        raise ValueError('$XDG_CONFIG_HOME: {} must be '
-                                         'somewhere within your home '
-                                         'directory: {}'
-                                         .format(xdg_config_home, home))
-                    if config.has_section('xdg_configuration_files'):
-                        for path in config.options('xdg_configuration_files'):
-                            if path.startswith('/'):
-                                raise ValueError('Unsupported absolute path: '
-                                                 '{}'
-                                                 .format(path))
-                            path = os.path.join(xdg_config_home, path)
-                            path = path.replace(home, '')
-                            (self.apps[app_name]['configuration_files']
-                                .add(path))
+                if not xdg_config_home.startswith(home):
+                    raise ValueError('$XDG_CONFIG_HOME: {} must be '
+                                     'somewhere within your home '
+                                     'directory: {}'
+                                     .format(xdg_config_home, home))
+                if config.has_section('xdg_configuration_files'):
+                    for path in config.options('xdg_configuration_files'):
+                        if path.startswith('/'):
+                            raise ValueError('Unsupported absolute path: '
+                                             '{}'
+                                             .format(path))
+                        path = os.path.join(xdg_config_home, path)
+                        path = path.replace(home, '')
+                        (self.apps[app_name]['configuration_files']
+                            .add(path))
 
     @staticmethod
     def get_config_files():

aa75eec3172113f02cde67cfe67926869c317842
Frank Hoffsümmer <hoffsummer@spotify.com>
2019-09-02 22:22:28 +0200

    Add script to generate list of supported Applications

---
+++ mackup/generate_supported_apps.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import glob
+import configparser
+import os
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+if __name__ == "__main__":
+    mdfilename = os.path.join(root, "../SUPPORTED_APPLICATIONS.md")
+    with open(mdfilename, 'w+') as mdfile:
+        applications = sorted(glob.glob("applications/*cfg"))
+        mdfile.write(f"## Supported Applications ({len(applications)})\n\n")
+        for cfg_file in applications:
+            # https://stackoverflow.com/a/52306763
+            config = configparser.ConfigParser(comment_prefixes='/', allow_no_value=True)
+            config.optionxform = str
+            config.read(cfg_file)
+            mdfile.write(f"- [{config['application']['name']}]({config['application']['homepage']})\n")

8182270800363098cc9c8dadfe0a31e00c3ba0d3
Grigorii Horos <horosgrisa@gmail.com>
2020-06-02 11:38:18 +0300

    Add posibility to set colors using LS_COLORS-like variable MACKUP_COLORS

--- mackup/config.py
+++ mackup/config.py
@@ -326,6 +326,27 @@ class Config(object):
                     "[" + str(self._parser.get("colors", "header_app_name")) + "m"
                 )
 
+        lsc = os.environ["MACKUP_COLORS"]
+        for setting in lsc.split(":"):
+            if not setting:
+                continue  # skip empty
+
+            key, code = setting.split("=")
+
+            if not key or not code:
+                continue  # skip empty
+
+            if key in [
+                "text",
+                "filename",
+                "filename_path_separator",
+                "name",
+                "item_bullet",
+                "item_header",
+                "header_app_name",
+            ]:
+                colors[key] = "[" + code + "m"
+
         return colors
 
 

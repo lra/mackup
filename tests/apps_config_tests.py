135f8373171844b5b2d8cacf27b4bd06b0abcaf7
Jim Neuendorf <jim.neuendorf@gmx.de>
2022-03-15 00:47:19 +0100

    support python 2.7, lint

--- tests/apps_config_tests.py
+++ tests/apps_config_tests.py
@@ -4,6 +4,12 @@ import unittest
 from unittest.mock import patch
 import stat
 
+try:
+    from unittest.mock import patch
+except ImportError:
+    patch = None
+
+
 from mackup import utils
 from mackup.appsdb import ApplicationsDatabase
 
@@ -39,19 +45,29 @@ class TestMackup(unittest.TestCase):
         os.remove(self.config_file_path)
 
     def test_glob_configuration_paths(self):
-        with patch.object(
-            ApplicationsDatabase,
-            "get_config_files",
-            return_value=[self.config_file_path],
-        ) as method:
+        if patch:
+            with patch.object(
+                ApplicationsDatabase,
+                "get_config_files",
+                return_value=[self.config_file_path],
+            ) as method:
+                app_db = ApplicationsDatabase()
+                self.assertEqual(
+                    app_db.get_files("test-app"),
+                    {
+                        "Library/Application Support/Test App/2020/data.txt",
+                        "Library/Application Support/Test App/2021/data.txt",
+                        "Library/Application Support/Test App/2022/data.txt",
+                    },
+                )
+        else:
             app_db = ApplicationsDatabase()
-            print(app_db.apps)
-
+            app_db.get_config_files = lambda *args, **kwargs: [self.config_file_path]
             self.assertEqual(
-                app_db.get_files("test-app"),
-                {
-                    "Library/Application Support/Test App/2020/data.txt",
-                    "Library/Application Support/Test App/2021/data.txt",
-                    "Library/Application Support/Test App/2022/data.txt",
-                },
-            )
+                    app_db.get_files("test-app"),
+                    {
+                        "Library/Application Support/Test App/2020/data.txt",
+                        "Library/Application Support/Test App/2021/data.txt",
+                        "Library/Application Support/Test App/2022/data.txt",
+                    },
+                )
